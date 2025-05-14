56f3a862e87214f32e68109b934218f2d375717f
yvonnemon <33262763+yvonnemon@users.noreply.github.com>
2025-05-14 09:53:05 +0200

    i18n js

---
+++ front/skybook-frontend/src/i18n.js
@@ -0,0 +1,14 @@
+import { createI18n } from 'vue-i18n'
+import en from './locales/en.json'
+import es from './locales/es.json'
+
+const i18n = createI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: {
+    en,
+    es
+  }
+})
+
+export default i18n
